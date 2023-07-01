4a410a359d2d92af26dd4a2d5f0717c892226586
Valerie <sleeplessval@gmail.com>
2023-07-01 16:23:36 -0400

    ergodox: added scroll lock key and unified leftmost column on num layer

--- ergodox/keymap.c
+++ ergodox/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,		KC_Q,		KC_W,		KC_E,		KC_R,		KC_T,		KC_BSPC,				KC_LBRC,	KC_Y,		KC_U,		KC_I,		KC_O,		KC_P,		KC_BSLS,
 		MO(1),		KC_A,		KC_S,		KC_D,		KC_F,		KC_G,											KC_H,		KC_J,		KC_K,		KC_L,		KC_SCLN,	KC_QUOT,
 		KC_LCTL,	KC_Z,		KC_X,		KC_C,		KC_V,		KC_B,		KC_SPC,					KC_RBRC,	KC_N,		KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	KC_ENT,
-		TD(TD_CAPS),KC_NO,	KC_NO,		KC_LGUI,	KC_LALT,																KC_RALT,	LSG(KC_S),	KC_RCTL,	KC_APP,		OSL(2),
+		TD(TD_CAPS),KC_NO,		KC_NO,		KC_LGUI,	KC_LALT,																KC_RALT,	LSG(KC_S),	KC_RCTL,	KC_APP,		OSL(2),
 
 																	KC_INS,		KC_BRK,					KC_HOME,	KC_END,
 																				LAG(KC_SPC),			KC_PGUP,
@@ -60,15 +60,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LSFT,	KC_NO,		KC_NO,		KC_TRNS,	KC_TRNS,																KC_TRNS,	KC_NO,		KC_TRNS,	KC_NO,		KC_NO,
 
 																	KC_NO,		KC_NO,					KC_MPRV,	KC_MNXT,
-																				KC_NO,					KC_NO,
+																				KC_NO,					KC_SCRL,
 														KC_MPLY,	KC_TRNS,	KC_TRNS,				KC_NO,		KC_TRNS,	KC_TRNS
 	),
 	[NUM] = LAYOUT_ergodox_pretty(
-		KC_TRNS,	KC_NO,		KC_PPLS,	KC_NO,		KC_NO,		KC_NO,		KC_NO,					KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_PPLS,	KC_NO,		KC_BSPC,
-		KC_TRNS,	KC_NO,		KC_PMNS,	KC_7,		KC_8,		KC_9,		KC_BSPC,				KC_BSPC,	KC_7,		KC_8,		KC_9,		KC_PMNS,	KC_NO,		KC_NO,
-		KC_NO,		KC_NO,		KC_PAST,	KC_4,		KC_5,		KC_6,											KC_4,		KC_5,		KC_6,		KC_PAST,	KC_NO,		KC_QUOT,
-		KC_LCTL,	KC_NO,		KC_PSLS,	KC_1,		KC_2,		KC_3,		KC_PENT,				KC_PENT,	KC_1,		KC_2,		KC_3,		KC_PSLS,	KC_SLSH,	KC_ENT,
-		KC_LSFT,	TG(BASE),	KC_NO,		KC_DOT,		KC_0,																	KC_0,		KC_DOT,		KC_NO,		KC_NO,		KC_NO,
+		KC_ESC,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_NO,					KC_NO,		KC_NO,		KC_NO,		KC_NO,		KC_PPLS,	KC_NO,		KC_BSPC,
+		KC_TAB,		KC_7,		KC_8,		KC_9,		KC_NO,		KC_NO,		KC_BSPC,				KC_BSPC,	KC_7,		KC_8,		KC_9,		KC_PMNS,	KC_NO,		KC_NO,
+		KC_LSFT,	KC_4,		KC_5,		KC_6,		KC_NO,		KC_NO,											KC_4,		KC_5,		KC_6,		KC_PAST,	KC_NO,		KC_QUOT,
+		KC_LCTL,	KC_1,		KC_2,		KC_3,		KC_NO,		KC_NO,		KC_PENT,				KC_PENT,	KC_1,		KC_2,		KC_3,		KC_PSLS,	KC_SLSH,	KC_ENT,
+		KC_NO,		TG(BASE),	KC_0,		KC_DOT,		KC_LALT,																KC_0,		KC_DOT,		KC_NO,		KC_NO,		KC_NO,
 
 																	KC_NO,		KC_NO,					KC_NO,		KC_NO,
 																				KC_NO,					KC_PGUP,
